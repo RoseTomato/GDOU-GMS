c8b1c38019c679e782b5d0d55a2824c601a3dc65
Rose <753653287@qq.com>
2015-11-18 20:58:05 +0800

    成功搭建spring-springMVC

--- gms/src/main/java/love/drose/gms/controllers/ManagerHandler.java
+++ gms/src/main/java/love/drose/gms/controllers/ManagerHandler.java
@@ -1,8 +1,19 @@
 package love.drose.gms.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * 管理员控制类
  * Created by lovedrose on 2015/11/18.
  */
+@Controller
+@RequestMapping("/manager")
 public class ManagerHandler {
+
+    @RequestMapping("/test")
+    public String test() {
+        System.out.println("test!!!");
+        return "success";
+    }
 }
